340c8b215308b3ab74ccb8628271faaef3b3092f
dajkagabi <dajkagabi01@gmail.com>
2023-02-14 00:02:56 +0100

    Update App.js
    List-ScrollView

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [name, setName] = useState([
+    { name: "Frank", key: "1" },
+    { name: "Jone", key: "2" },
+    { name: "Hola", key: "3" },
+    { name: "Peter", key: "4" },
+    { name: "Roues", key: "5" },
+    { name: "Gilbert", key: "6" },
+
+
+    { name: "Uld", key: "7" },
+    { name: "Kavics", key: "8" },
+    { name: "Pek", key: "9" },
+    { name: "Hal", key: "10" }
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <ScrollView>
+        {name.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -14,7 +39,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    /* alignItems: 'center',
+     justifyContent: 'center', */
+    marginTop: 20
   },
+  item: {
+    fontSize: 18,
+    color: "blue",
+    backgroundColor: "gainsboro",
+    margin: 18,
+    padding: 18
+
+  }
 });
